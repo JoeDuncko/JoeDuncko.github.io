49b0cb97eaa0aab8379af3a62aa0e3545f5cda0b
Joe Duncko <personal@joeduncko.com>
2015-09-16 10:25:53 -0400

    Added print.css

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,14 @@
 //add a link icon to the right of every link
 //turn it blue when you mouse over the link text
 $(function() {
-    $( "a" ).hover(
+    $("a").hover(
         function() {
-            $( this ).children(".fa-external-link").addClass( "blue" );
+            $(this).children(".fa-external-link").addClass("blue");
         },
         function() {
-            $( this ).children(".fa-external-link").removeClass( "blue" );
+            $(this).children(".fa-external-link").removeClass("blue");
         }
     );
 
-    $( "a" ).append("<i class='fa fa-external-link'></i>");
+    $("a:not(.do-not-add-link-icon)").append("<i class='fa fa-external-link hide-when-printing'></i>");
 });
